12d557ad9196523b9573a5397978642725559624
Nikita Maslenko <nickita0098@gmail.com>
2024-04-12 10:52:21 +0200

    add things generate

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 # main.py
+from random import randint, choice
+
 class Thing:
-    def __init__ (name, defense, attack, health)
+    """Вещи: class Thing Класс содержит в себе следующие параметры - название,
+    процент защиты, атаку и жизнь; Это могут быть предметы одежды, магические
+    кольца, всё что угодно)"""
+    default_names = ['thing1, thing2, thing3, thing4, thing5']
+
+    def __init__ (self, name, defense, attack, health):
+        self.name = name
+        self.defense = defense
+        self.attack = attack
+        self.health = health
+
+#Алгоритм проведения боя
+def main():
+    pass
+    """
+    Шаг 1 - создаем произвольное количество вещей с различными параметрами,
+    процент защиты не должен превышать 10%(0.1). Сортируем по проценту защиты,
+    по возрастанию;
+    """
+things_list = []
+
+for i in range(randint(2, 5)):
+    name = choice(Thing.default_names)
+    defense = randint(0, 10)/100
+    attack = randint(0, 100)/100
+    health = randint(0, 100)/100
+    things_list.append(Thing(name, defense, attack, health))
+    print(things_list)
